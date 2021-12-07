b1e58b5e3a6d31eb4bb377afe731fea1f76fd967
Abdul <AMalik@lcs.on.ca>
2021-12-07 14:35:35 -0500

    Half of the question interface has been built in a static manner.

---
+++ FlashCards/Model/Card.swift
@@ -0,0 +1,39 @@
+//
+//  Card.swift
+//  FlashCards
+//
+//  Created by Abdul Malik on 2021-12-07.
+//
+
+import Foundation
+
+struct Card {
+    
+    let question: String
+    let answer: String
+    
+}
+
+let listOfCards = [
+    
+    Card(question: "What's \"powerhouse\" of a cell?",
+         answer: "mitrochondria")
+    
+    ,
+    
+    Card(question: "What is the largest bone in your body?",
+         answer: "Femur")
+    
+    ,
+    
+    Card(question: "What is the acceleration of the gravitational pull?",
+         answer: "9.80 m/s squared")
+    
+    
+    
+    
+    
+    
+    
+    
+]
