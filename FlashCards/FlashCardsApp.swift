a34d1fb665838be69d2be7f56d398f95528f2813
Abdul <AMalik@lcs.on.ca>
2021-12-07 14:14:44 -0500

    Initial Commit

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by Abdul Malik on 2021-12-07.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
